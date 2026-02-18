dbac239f5b6d3b28c7212a615b3698ec8823b7aa
Andres Bedoya <mortiis.angel@gmail.com>
2026-02-18 09:39:16 -0500

    Initial Commit

---
+++ ProjectManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjectManager
+//
+//  Created by Andres Bedoya on 18/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Andres Bedoya on 18/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
